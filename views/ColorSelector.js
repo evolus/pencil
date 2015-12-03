aa29fcfcdd058efb7a524990c1e985d628afc7a8
Pham Ngoc Van Anh <anh.pham@evolus.vn>
2015-12-03 11:32:24 +0700

    update color selector

--- views/ColorSelector.js
+++ views/ColorSelector.js
@@ -17,8 +17,66 @@ function ColorSelector() {
     //register the event handler
     // wheel selector event handler
     this.htmlCodeInput.addEventListener("change", function(event) {
+        var val = thiz.htmlCodeInput.value;
+        var uppercaseVal = val.toRGBString ? val.toRGBString().toUpperCase() : (val.toUpperCase ? val.toUpperCase() : val);
+        // Translate standard HTML color strings:
+        if (uppercaseVal[0] != "#") {
+            switch (val) {
+                case "GREEN":
+                    uppercaseVal = "#008000";
+                    break;
+                case "LIME":
+                    uppercaseVal = "#00FF00";
+                    break;
+                case "OLIVE":
+                    uppercaseVal = "#808000";
+                    break;
+                case "TEAL":
+                    uppercaseVal = "#008080";
+                    break;
+                case "YELLOW":
+                    uppercaseVal = "#FFFF00";
+                    break;
+                case "RED":
+                    uppercaseVal = "#FF0000";
+                    break;
+                case "MAROON":
+                    uppercaseVal = "#800000";
+                    break;
+                case "PURPLE":
+                    uppercaseVal = "#800080";
+                    break;
+                case "FUCHSIA":
+                    uppercaseVal = "#FF00FF";
+                    break;
+                case "NAVY":
+                    uppercaseVal = "#000080";
+                    break;
+                case "BLUE":
+                    uppercaseVal = "#0000FF";
+                    break;
+                case "AQUA":
+                    uppercaseVal = "#00FFFF";
+                    break;
+                case "WHITE":
+                    uppercaseVal = "#FFFFFF";
+                    break;
+                case "SILVER":
+                    uppercaseVal = "#CC0C0C0";
+                    break;
+                case "GRAY":
+                    uppercaseVal = "#808080";
+                    break;
+                default: // BLACK
+                    uppercaseVal = "#000000";
+                    break;
+            }
+        } else if (uppercaseVal.length > 7) {
+            uppercaseVal = uppercaseVal.substring(7);
+        }
+
         var a = thiz.color.a;
-        thiz.color = Color.fromString(thiz.htmlCodeInput.value);
+        thiz.color = Color.fromString(uppercaseVal);
         thiz.color.a = a;
         thiz.onValueChanged(thiz.htmlCodeInput);
     }, false);
