d998b512253b1d7898553c317c51ab590af96c25
Duong Thanh An <an.duong@evolus.vn>
2015-12-10 08:05:30 +0700

    Fix upgradeMDL

--- views/ApplicationPane.js
+++ views/ApplicationPane.js
@@ -1,8 +1,5 @@
 function ApplicationPane() {
     BaseTemplatedWidget.call(this);
-
-    componentHandler.upgradeElements([this.testMenuButton, this.testMenuButtonList]);
-
     Pencil.boot();
 }
 __extend(BaseTemplatedWidget, ApplicationPane);
