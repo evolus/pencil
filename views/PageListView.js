1a80b9c4580bc03bdbabb7f98801888349085960
Pham Ngoc Van Anh <anh.pham@evolus.vn>
2016-02-24 11:30:06 +0700

    update page list view

--- views/PageListView.js
+++ views/PageListView.js
@@ -9,23 +9,28 @@ function PageListView() {
     }
     this.bind("click", function (event) {
         var node = findPageThumbnailView(event);
-        this.handleSelectPage(node);
+        if (!node) return;
+        this.handleSelectPage(node.__widget.page);
     }, this.pageListContainer);
 
     this.bind("dblclick", function (event) {
         var node = findPageThumbnailView(event);
         if (!node) return;
-        var view = node.__widget;
-        var page = view.page;
-        if (!page.children || page.children.length == 0) {
-            this.handleSelectPage(node);
-        } else {
-            this.currentParentPage = page;
-            this.currentPage = null;
-            this.renderPages();
-        }
+        this.handleDoubleClick(node.__widget.page);
 
     }, this.pageListContainer);
+
+    this.bind("click", function (event) {
+        var page = Dom.findUpwardForData(event.target, "_page");
+        if (!page) return;
+        this.handleSelectPage(page);
+    }, this.childPageContainer);
+
+    this.bind("dblclick", function (event) {
+        var page = Dom.findUpwardForData(event.target, "_page");
+        if (!page) return;
+        this.handleDoubleClick(page);
+    }, this.childPageContainer);
 
     this.bind("click", function (event) {
         var node = Dom.findUpward(event.target, function (n) {
@@ -68,9 +73,16 @@ function PageListView() {
             }
         });
     }, this.addPageButton);
+
+    this.bind("click", function (event) {
+        this.expanded = !this.expanded;
+        this.toggle();
+        Config.set("pageListViewExpanded", this.expanded);
+    }, this.toggleButton);
+
+    this.expanded = Config.get("pageListViewExpanded", "true") == "true" ? true : false;
 }
 __extend(BaseTemplatedWidget, PageListView);
-
 PageListView.prototype.setController = function (controller) {
     this.controller = controller;
     this.currentParentPage = null;
@@ -85,6 +97,8 @@ PageListView.prototype.activatePage = function (page) {
 PageListView.prototype.renderPages = function() {
     this.pageBreadcrumb.innerHTML = "";
     this.pageListContainer.innerHTML = "";
+    this.childPageContainer.innerHTML = "";
+
     this.views = [];
     if (!this.controller || !this.controller.pages) return;
 
@@ -118,7 +132,7 @@ PageListView.prototype.renderPages = function() {
             },
             {
                 _name: "span",
-                _text: this.controller.getDocumentName()
+                _text: this.controller.getDocumentName() + (parentPages.length == 0 ? ":" : "")
             }
         ]
     });
@@ -132,7 +146,7 @@ PageListView.prototype.renderPages = function() {
             _children: [
                 {
                     _name: "span",
-                    _text: p.name
+                    _text: p.name + ((i == (parentPages.length - 1)) ? ":" : "")
                 }
             ]
         });
@@ -148,11 +162,39 @@ PageListView.prototype.renderPages = function() {
         this.pageListContainer.appendChild(pageThumbnailView.node());
         pageThumbnailView.selectPage(this.currentPage && this.currentPage.id == page.id);
         this.views.push(pageThumbnailView);
+
+        var childNode = Dom.newDOMElement({
+            _name: "hbox",
+            "selected": (this.currentPage && this.currentPage.id == page.id),
+            _children: [
+                {
+                    _name: "span",
+                    _text: page.name
+                }
+            ]
+        });
+
+        childNode._page = page;
+        this.childPageContainer.appendChild(childNode);
     }
 
+    this.toggle();
     this.controller.activatePage(this.currentPage);
-
 };
+
+PageListView.prototype.toggle = function() {
+    if (this.expanded) {
+        this.pageListContainer.style.display = "flex";
+        this.childPageContainer.style.display = "none";
+        this.toggleButton.childNodes[0].innerHTML = "expand_less";
+    } else {
+        var h = this.pageListContainer.offsetHeight;
+        this.pageListContainer.style.display = "none";
+        this.childPageContainer.style.display = "flex";
+        this.toggleButton.childNodes[0].innerHTML = "expand_more";
+    }
+};
+
 PageListView.prototype.handlePageInfoChangedEvent = function (event) {
     if (!event || !event.page) return;
     for (var i in this.views) {
@@ -163,13 +205,29 @@ PageListView.prototype.handlePageInfoChangedEvent = function (event) {
     }
 };
 
-PageListView.prototype.handleSelectPage = function (node) {
-    if (!node) return;
+PageListView.prototype.handleSelectPage = function (page) {
+    if (!page) return;
     Dom.doOnAllChildren(this.pageListContainer, function (n) {
         var view = n.__widget;
         if (!view) return;
-        view.selectPage(n == node);
+        var p = view.page;
+        view.selectPage(p.id == page.id);
     });
 
-    this.controller.activatePage(node.__widget.page);
+    Dom.doOnAllChildren(this.childPageContainer, function (n) {
+        var p = n._page;
+        n.setAttribute("selected", p.id == page.id);
+    });
+
+    this.controller.activatePage(page);
+};
+
+PageListView.prototype.handleDoubleClick = function (page) {
+    if (!page.children || page.children.length == 0) {
+        this.handleSelectPage(page);
+    } else {
+        this.currentParentPage = page;
+        this.currentPage = null;
+        this.renderPages();
+    }
 };
