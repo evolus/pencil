c47db9e4687ee0df2d7dfba11280f94027c805e2
Chi Cuong <cuong@localhost.localdomain>
2016-03-22 14:47:18 +0700

    fix icon button hidden collection

--- views/ShowHiddenCollectionDialog.js
+++ views/ShowHiddenCollectionDialog.js
@@ -1,58 +1,61 @@
-function ShowHiddenCollectionDialog(collectionPanel) {
+function ShowHiddenCollectionDialog (collectionPanel) {
     Dialog.call(this);
+    this.hiddenCollections = this.getHiddenCollection();
+    if(this.hiddenCollections.length == 0) {
+        this.close();
+    }
     this.collectionPanel = collectionPanel;
     this.bind("click", this.handleActionClick, this.dialogHeadCommand);
     this.title = function () {
-        return "Hidden Collection: ";
+        return "Hidden Collection:";
     };
     this.hiddenCollections = this.getHiddenCollection();
     var thiz = this;
      for( var i = 0; i < this.hiddenCollections.length; i++) {
         this.collectionContainer.appendChild(this.createCollectionButton(this.hiddenCollections[i]));
     }
-    this.collectionContainer.addEventListener("click",function(event) {
+    this.collectionContainer.addEventListener("click",function (event) {
         var node = Dom.findUpwardForNodeWithData(event.target,"_collection");
         var check = node.getAttribute("selected");
         if(check == "true") {
-            node.setAttribute("selected","false");
+            node.setAttribute("selected", "false");
         } else {
-            node.setAttribute("selected","true");
+            node.setAttribute("selected", "true");
         };
-    },false);
+    }, false);
 }
 __extend(Dialog, ShowHiddenCollectionDialog);
-
 
 ShowHiddenCollectionDialog.prototype.getCollectionIcon = function (collection) {
     return collection.icon || CollectionPane.ICON_MAP[collection.id] || "border_all";
 };
 
-ShowHiddenCollectionDialog.prototype.createCollectionButton = function(collection) {
+ShowHiddenCollectionDialog.prototype.createCollectionButton = function (collection) {
     var thiz = this;
     var icon = this.getCollectionIcon(collection);
     var button = Dom.newDOMElement({
         _name: "button",
         _children: [
-                            {
-                                _name: "i",
-                                _text: icon,
-                            },
-                            {
-                                _name: "br",
-                            },
-                            {
-                                _name: "span",
-                                _text: collection.displayName
-                            } 
-                        ]
+            {
+                _name: "i",
+                _text: icon,
+            }, 
+            {
+                _name: "br",
+            }, 
+            {
+                _name: "span",
+                _text: collection.displayName
+            } 
+        ]
     });
     button._id = collection.displayName;
     button._collection = collection;
-    button.setAttribute("selected","false");
+    button.setAttribute("selected", "false");
     return button;
 }
 
-ShowHiddenCollectionDialog.prototype.getHiddenCollection = function() {
+ShowHiddenCollectionDialog.prototype.getHiddenCollection = function () {
     var collections = CollectionManager.shapeDefinition.collections;
     var hiddenCollections = [];
     for (var i = 0; i < collections.length; i++) {
@@ -64,7 +67,7 @@ ShowHiddenCollectionDialog.prototype.getHiddenCollection = function() {
 }
 ShowHiddenCollectionDialog.prototype.getDialogActions = function () {
     return [
-        {   type: "accept", title: "Show", run: function() {
+        {   type: "accept", title: "Show", run: function () {
             if(this.hiddenCollections.length > 0) {
                 var node = this.collectionContainer;
                 for( var i = 0; i < node.children.length; i++){
@@ -76,11 +79,11 @@ ShowHiddenCollectionDialog.prototype.getDialogActions = function () {
                 this.collectionPanel.reload();
             }
             return true;
-        }},
-        {   type: "accept", title: "Uninstall Collections", run: function() {
+        }}, 
+        {   type: "accept", title: "Uninstall Collections", run: function () {
             return true;
-        }},
-        {   type: "extra1", title: "Install New Collection", run: function() {
+        }}, 
+        {   type: "extra1", title: "Install New Collection", run: function () {
             return true;
         }}
     ]
