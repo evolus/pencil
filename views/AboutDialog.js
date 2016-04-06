762facf4bbaa3c8405268f18faaa64f90c90eda3
Chi Cuong <cuong@localhost.localdomain>
2016-04-06 11:03:08 +0700

    Create About Pencil Dialog

--- views/AboutDialog.js
+++ views/AboutDialog.js
@@ -46,3 +46,7 @@ AboutDialog.prototype.getDialogActions = function () {
         }
     ]
 };
+window.addEventListener("load", function () {
+    var dialog = new AboutDialog();
+    dialog.open();
+},false)
