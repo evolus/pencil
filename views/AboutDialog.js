4503c021e61c51f926d9f2180e7eacf92f7f1dbe
Chi Cuong <cuong@localhost.localdomain>
2016-04-06 14:46:50 +0700

    Create About Pencil Dialog

--- views/AboutDialog.js
+++ views/AboutDialog.js
@@ -6,36 +6,17 @@ function AboutDialog () {
 
 __extend(Dialog, AboutDialog);
 
+AboutDialog.prototype.getLicense = function (thiz) {
+
+}
 AboutDialog.prototype.setup = function (options) {
     fs.readFile("pencil-core/license.txt", 'utf8', (err, data) => {
         if (err) throw err;
         this.licenseText.value = data;
     });
-    for (var i = 0; i < this.tabContainer.children.length; i++) {
-        if (this.tabContainer.children[i].hasAttribute("active") == true) {
-            this.tabCurrentActive = this.tabContainer.children[i];
-        }
-    }
-    var thiz = this;
-    this.tabSelector.addEventListener("click", function (event) {
-        var tab = event.target.getAttribute("active");
-        if (!tab) {
-            tab = event.target.getAttribute("name");
-            switch (tab) {
-                case "tab1":
-                thiz.tab1.style.zIndex = 30;
-                thiz.tab2.style.zIndex = 20;
-                break;
-                case "tab2":
-                thiz.tab1.style.zIndex = 20;
-                thiz.tab2.style.zIndex = 30;
-                break;
-            }
-            event.target.setAttribute("active","true");
-            thiz.tabCurrentActive.removeAttribute("active");
-            thiz.tabCurrentActive = event.target;
-        }
-    },false);
+    this.tabHeader.addTab("Creadit", this.aboutTab1);
+    this.tabHeader.addTab("License", this.aboutTab2);
+
 }
 
 AboutDialog.prototype.getDialogActions = function () {
