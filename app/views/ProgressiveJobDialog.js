e22a7978c4e5f885e78d42adb1e8c3aa29bac54d
Duong Thanh An <an.duong@evolus.vn>
2016-05-17 13:59:29 +0700

    adding export progress dialog

---
+++ app/views/ProgressiveJobDialog.js
@@ -0,0 +1,36 @@
+function ProgressiveJobDialog () {
+    Dialog.call(this);
+}
+
+__extend(Dialog, ProgressiveJobDialog);
+
+ProgressiveJobDialog.prototype.setup = function (options) {
+    this.options = options || {};
+    this.starter = this.options.starter;
+    this.title = this.options.title || "Progress";
+    this.onProgressUpdated("Starting...", 0, 1);
+    this.starter(this);
+}
+ProgressiveJobDialog.prototype.getDialogActions = function () {
+    var thiz = this;
+    return [
+        {
+            type: "cancel", title: "Cancel",
+            isCloseHandler: true,
+            isApplicable: function () {
+                return thiz.options.cancelable;
+            },
+            run: function () { return true; }
+        }
+    ]
+};
+
+ProgressiveJobDialog.prototype.onTaskDone = function () {
+    console.log("onTaskDone called");
+    this.close();
+};
+ProgressiveJobDialog.prototype.onProgressUpdated = function (status, completed, total) {
+    console.log("onProgressUpdated called", [status, completed, total]);
+    this.statusLabel.innerHTML = Dom.htmlEncode(status || "Please wait...");
+    this.progressBarInner.style.width = Math.round(100 * completed / total) + "%";
+};
