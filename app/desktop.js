789bf4054d113a298f3e82a568c3874e70496a91
Nguyen Tien Dzung <ngtdungnt@gmail.com>
2016-06-08 17:56:20 +0700

    desktop font config: adding support for ubuntu.

--- app/desktop.js
+++ app/desktop.js
@@ -8,6 +8,7 @@ module.exports = function () {
                                 "cinamon": [{command: "dconf", params: ["read", "/org/cinnamon/desktop/interface/font-name"]},
                                             {command: "gsettings", params: ["get", "org.cinnamon.desktop.interface", "font-name"]}],
                                 "gnome": [{command: "gsettings", params: ["get", "org.gnome.desktop.interface", "font-name"]}],
+                                "ubuntu": [{command: "gsettings", params: ["get", "org.gnome.desktop.interface", "font-name"]}]
                             }
     var fontNameReader = function(registry, callback, index) {
         var child = spawn(registry.command, registry.params);
@@ -62,6 +63,9 @@ module.exports = function () {
     var platformHandlers = {
         linux: function (callback) {
             var d = process.env.DESKTOP_SESSION;
+            if (/ubuntu/ig.exec(d)) {
+                d = "ubuntu";
+            }
             var fontRegistry = fontCommandRegistry[d];
             if (!fontRegistry) {
                 console.error("Coud not found font command registry for ", d);
