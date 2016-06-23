48e2c976700a6d965c31c33e9a845ae68816a9e4
ngo chi cuong <cuong.ngo@evolus.vn>
2016-06-23 13:47:35 +0700

    fixed quick drag and drop shape into canvas

--- app/pencil-core/common/Canvas.js
+++ app/pencil-core/common/Canvas.js
@@ -308,7 +308,6 @@ function Canvas(element) {
         }
     }.bind(this));
 
-
 }
 
 SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(elem) {
@@ -671,7 +670,6 @@ Canvas.prototype.insertShapeImpl_ = function (shapeDef, bound,
     this.snappingHelper.updateSnappingGuide(this.currentController);
     DockingManager.enableDocking(this.currentController);
 
-
 };
 Canvas.prototype.selectShape = function (shape) {
 
@@ -813,7 +811,6 @@ Canvas.prototype.finishMoving = function (event) {
             clientY : event.clientY
         });
     }
-
 };
 Canvas.prototype.handleMouseWheel = function(event) {
     if (event.ctrlKey) {
@@ -2267,6 +2264,7 @@ Canvas.prototype.setMemento = function (memento) {
 };
 Canvas.prototype.run = function (job, targetObject, actionName, args) {
 
+    console.log("add undo", actionName);
     try {
         // console.log();
         job.apply(targetObject, args);
@@ -2689,8 +2687,6 @@ Canvas.prototype.__dragenter = function (event) {
     }
 };
 Canvas.prototype.__dragleave = function (event) {
-    this.element.removeAttribute("is-dragover");
-    this.element.removeAttribute("p:holding");
     // this.element.removeAttribute("p:selection");
     if (!this.currentDragObserver)
         return;
@@ -2699,6 +2695,8 @@ Canvas.prototype.__dragleave = function (event) {
     } catch (e) {
         Console.dumpError(e);
     }
+    this.element.removeAttribute("is-dragover");
+    this.element.removeAttribute("p:holding");
 };
 Canvas.prototype.__dragend = function (event) {
     this.element.removeAttribute("is-dragover");
