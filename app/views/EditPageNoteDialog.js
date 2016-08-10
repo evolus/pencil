e139a5a2cec23eecd5f8140bd9c54e67be6c942f
ngo chi cuong <cuong.ngo@evolus.vn>
2016-08-10 10:37:14 +0700

    sketchy-UI: update table && edit note page dialog

--- app/views/EditPageNoteDialog.js
+++ app/views/EditPageNoteDialog.js
@@ -106,7 +106,7 @@ function EditPageNoteDialog () {
             return n.getAttribute && n.getAttribute("command");
         });
         if (!node) return;
-        var command = node.getAttribute("command");
+        var command = node.getAttribute("command");ns
         var arg = node.hasAttribute("arg") ? node.getAttribute("arg") : undefined;
         if(command == "createlink") {
             var sel = window.document.getSelection();
@@ -203,8 +203,7 @@ function EditPageNoteDialog () {
         }
         thiz.runEditorCommand("formatBlock", value);
     }, false);
-
-    //this.setup();
+    
 }
 __extend(Dialog, EditPageNoteDialog);
 
