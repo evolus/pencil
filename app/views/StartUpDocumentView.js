6234b20dee70e094d01ac705101d46e223b0c7f0
ngo chi cuong <cuong.ngo@evolus.vn>
2016-08-22 11:06:21 +0700

    make control to change view style for recent Document

--- app/views/StartUpDocumentView.js
+++ app/views/StartUpDocumentView.js
@@ -1,13 +1,24 @@
 function StartUpDocumentView() {
     BaseTemplatedWidget.call(this);
 
-    this.recentDocumentRepeater.populator = function (doc, binding) {
+    var gridViewCheck = Config.get("view.recentGridView");
+    if (gridViewCheck == null) {
+        Config.set("view.recentGridView", true);
+        gridViewCheck = true;
+    }
+    if (gridViewCheck) this.currentRepeater = this.recentDocumentRepeater;
+    else this.currentRepeater = this.recentDocumentRepeaterRow;
+
+    this.currentRepeater.populator = function (doc, binding) {
         var filePath = doc.filePath;
         var handler = function (error, thumbPath) {
             var stats = fs.statSync(filePath);
             if (stats) {
                 binding.name.innerHTML = Dom.htmlEncode(path.basename(filePath));
                 binding.info.innerHTML = Dom.htmlEncode(moment(stats.mtime).fromNow());
+                if (binding.path) {
+                    binding.path.innerHTML = Dom.htmlEncode(filePath);
+                }
                 if (thumbPath) {
                     window.setTimeout(function () {
                         Util.setupImage(binding.thumbnailImage, ImageData.filePathToURL(thumbPath), "center-crop", "allowUpscale");
@@ -23,11 +34,34 @@ function StartUpDocumentView() {
         } else {
             Pencil.controller.parseDocumentThumbnail(filePath, handler);
         }
-    };
+    }
 
+    var thiz = this;
     this.bind("click", function (event) {
+        var node = Dom.findUpward(event.target, function(node) {
+            if (node.getAttribute("command") == "pinDocument") return true;
+            return false;
+        })
+
         var filePath = Dom.findUpwardForData(event.target, "_filePath");
         if (!filePath) return;
+
+        if (node) {
+            var bookFiles = Config.get("favorite-documents");
+            for (var i = 0; i < bookFiles.length; i++) {
+                if (bookFiles[i] == filePath) {
+                    bookFiles.splice(i,1);
+                    break;
+                }
+            }
+            Config.set("favorite-documents", bookFiles);
+
+            var recentFile = Config.get("recent-documents");
+            recentFile.push(filePath);
+            Config.set("recent-document", recentFile);
+            thiz.reload();
+            return;
+        }
 
         function handler() {
             Pencil.controller.loadDocument(filePath);
@@ -38,7 +72,7 @@ function StartUpDocumentView() {
         }
         handler();
 
-    }, this.recentDocumentRepeater);
+    }, this.currentRepeater);
 
     Dom.doOnAllChildRecursively(this.node(), function (n) {
         if (!n.getAttribute || !n.getAttribute("command")) return;
@@ -52,30 +86,52 @@ __extend(BaseTemplatedWidget, StartUpDocumentView);
 
 StartUpDocumentView.prototype.reload = function () {
     var files = Config.get("recent-documents");
+    var favoriteFiles = Config.get("favorite-documents");
     var map = Config.get("recent-documents-thumb-map") || {};
-    var docs = [];
-    var deletedFiles = [];
-    if (files) {
+    var recentDocs = [];
+    var favoDocs = [];
+    var loadFiles = function (files, deletedFiles) {
+        var doc = [];
         for (var i = 0; i < Math.min(files.length, 8); i++) {
             var checkExist = fs.existsSync(files[i]);
             if(!checkExist) {
                 deletedFiles.push(files[i]);
             } else {
-                docs.push({
+                doc.push({
                     filePath: files[i],
                     thumbPath: map[files[i]] || null
                 });
             }
         }
-        for(i in deletedFiles) {
-            Pencil.controller.removeRecentFile(deletedFiles[i]);
+        return doc;
+    }
+    var deleteFiles = [];
+    if (files) {
+        recentdocs = loadFiles(files, deleteFiles);
+        if (deleteFiles.length > 0) {
+            for (var i = 0; i < deleteFiles.length; i++) {
+                Pencil.controller.removeRecentFile(deleteFiles[i]);
+            }
+        }
+    }
+
+    deleteFiles = [];
+    if (favoriteFiles) {
+        favoDocs = loadFiles(favoriteFiles, deleteFiles);
+        if (deleteFiles.length > 0) {
+            for (var i = 0; i < deleteFiles.length; i++) {
+                favoriteFiles.splice(i, 1);
+            }
+            Config.set("favorite-documents", favoriteFiles);
         }
     }
 
     var thiz = this;
-    this.recentDocumentRepeater.node().style.visibility = "hidden";
+    this.currentRepeater.node().style.visibility = "hidden";
+    // reload Favorite Documents
+
     setTimeout(function () {
-        thiz.recentDocumentRepeater.setItems(docs);
-        thiz.recentDocumentRepeater.node().style.visibility = "inherit";
+        thiz.currentRepeater.setItems(recentdocs);
+        thiz.currentRepeater.node().style.visibility = "inherit";
     }, 200);
 };
