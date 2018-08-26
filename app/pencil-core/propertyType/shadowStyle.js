d89d2ae65b2aa2b7917420288f03314324d16a32
Duong Thanh An <an.duong@evolus.vn>
2018-08-26 17:09:59 +0700

    Add support for shadow color in stencil generated from stencil builder

--- app/pencil-core/propertyType/shadowStyle.js
+++ app/pencil-core/propertyType/shadowStyle.js
@@ -3,8 +3,12 @@ function ShadowStyle() {
     this.dy = 0;
     this.size = 3;
     this.opacity = 1;
+    this.color = ShadowStyle.DEFAULT_COLOR;
 }
-ShadowStyle.REG_EX = /^([^\|]+)\|([^\|]+)\|([^\|]+)(\|([^\|]+))?$/i;
+
+ShadowStyle.DEFAULT_COLOR = "#000000";
+
+ShadowStyle.REG_EX = /^([^\|]+)\|([^\|]+)\|([^\|]+)(\|([^\|]+))?(\|([^\|]+))?$/i;
 ShadowStyle.fromString = function (literal) {
     var shadowStyle = new ShadowStyle();
     if (literal.match(ShadowStyle.REG_EX)) {
@@ -13,15 +17,17 @@ ShadowStyle.fromString = function (literal) {
         shadowStyle.size = parseFloat(RegExp.$3);
         var o = RegExp.$5;
         shadowStyle.opacity = o ? parseFloat(o) : 1.0;
+        var c = RegExp.$7;
+        shadowStyle.color = c || ShadowStyle.DEFAULT_COLOR;
     }
 
     return shadowStyle;
 };
 ShadowStyle.prototype.toString = function () {
-    return [this.dx, this.dy, this.size, this.opacity].join('|');
+    return [this.dx, this.dy, this.size, this.opacity, this.color || ShadowStyle.DEFAULT_COLOR].join('|');
 };
 ShadowStyle.prototype.toCSSString = function (color) {
-    var css = [this.dx + "px", this.dy + "px", Math.abs(this.size) + "px", color.toRGBAString()].join(" ");
+    var css = [this.dx + "px", this.dy + "px", Math.abs(this.size) + "px", color ? color.toRGBAString() : (this.color || ShadowStyle.DEFAULT_COLOR)].join(" ");
     if (this.size < 0) css = "inset " + css;
     return css;
 };
--- app/views/editors/ShadowStyleEditor.js
+++ app/views/editors/ShadowStyleEditor.js
@@ -1,10 +1,42 @@
 function ShadowStyleEditor() {
     PropertyEditor.call(this);
+    
+    var thiz = this;
+    this.selectorContainer.shouldCloseOnBlur = function(event) {
+        var found = Dom.findUpward(event.target, function (node) {
+            return node == thiz.colorButton;
+        });
+        return !found;
+    };
 }
 __extend(PropertyEditor, ShadowStyleEditor);
 
 ShadowStyleEditor.prototype.setup = function () {
     var thiz = this;
+    
+    this.colorButton.addEventListener("click", function (event) {
+        if (thiz.selectorContainer.isVisible()) {
+            thiz.selectorContainer.hide();
+            return;
+        }
+        thiz.selector.setColor(Color.fromString(thiz.color));
+        thiz.selectorContainer.show(thiz.colorButton, "left-inside", "bottom", 0, 5);
+        event.cancelBubble = true;
+    }, false);
+
+    this.selector.addEventListener("ValueChange", function (event) {
+        thiz.color = thiz.selector.getColor().toRGBString();
+        thiz.invalidateColorDisplay();
+        thiz.fireChangeEvent();
+    }, false);
+
+    this.selector.addEventListener("p:CloseColorSelector", function (event) {
+        if (thiz.selectorContainer.isVisible()) {
+            thiz.selectorContainer.hide();
+            return;
+        }
+    }, false);
+
     this.node().addEventListener("change", function (event) {
         thiz.fireChangeEvent();
     }, false);
@@ -15,6 +47,11 @@ ShadowStyleEditor.prototype.setValue = function (shadowStyle) {
     this.dy.value = shadowStyle.dy;
     this.size.value = shadowStyle.size;
     this.opacity.value = shadowStyle.opacity;
+    this.color = shadowStyle.color || "#000000";
+    this.invalidateColorDisplay();
+};
+ShadowStyleEditor.prototype.invalidateColorDisplay = function () {
+    this.colorButton.style.background = this.color;
 };
 ShadowStyleEditor.prototype.getValue = function () {
     var shadowStyle = new ShadowStyle();
@@ -22,5 +59,6 @@ ShadowStyleEditor.prototype.getValue = function () {
     shadowStyle.dy = this.dy.value;
     shadowStyle.size = this.size.value;
     shadowStyle.opacity = this.opacity.value;
+    shadowStyle.color = this.color;
     return shadowStyle;
 };
