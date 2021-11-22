c63c14c0ca0244d960209441af35990955259674
Duong Thanh An <an.duong@evolus.vn>
2021-11-22 17:55:48 +0700

    Add utilities for calculating color contrast

--- app/pencil-core/propertyType/color.js
+++ app/pencil-core/propertyType/color.js
@@ -208,6 +208,25 @@ Color.prototype.getDiff = function (other) {
             + (Math.abs(hsv0.value - hsv1.value) / (100 * 4))
             + (Math.abs(this.a - other.a) / 4);
 }
+Color.prototype.getContrastTo = function (other) {
+    var lum1 = Color.luminance(this.r, this.g, this.b);
+    var lum2 = Color.luminance(other.r, other.g, other.b);
+    var brightest = Math.max(lum1, lum2);
+    var darkest = Math.min(lum1, lum2);
+    var c = (brightest + 0.05)
+         / (darkest + 0.05);
+
+    return c * this.a * other.a;
+};
+Color.luminance = function (r, g, b) {
+    var a = [r, g, b].map(function (v) {
+        v /= 255;
+        return v <= 0.03928
+            ? v / 12.92
+            : Math.pow( (v + 0.055) / 1.055, 2.4 );
+    });
+    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+};
 
 pencilSandbox.Color = {
     newColor: function () {
